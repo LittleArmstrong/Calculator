f98e5319780a2e7de8929cfbf5636eb921b02e3c
LittleArmstrong <59065008+LittleArmstrong@users.noreply.github.com>
2022-06-02 16:19:50 +0200

    Add simple calculation and clear all function
    Add simple calculation and clear all function

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,125 @@
-let current_operation = "";
+import { $ } from "./toolbox.mjs";
 
-function calculate_operation() {
-   let str = "10+10";
-   if (!is_valid_operation(str)) return error("Invalid expression");
-   let [status, result] = calculate(str);
+const display = $("#display");
+
+// try FSM for creating string expressiongi
+function bind_events() {
+   //add the numbers to the display if pressed
+   for (let i = 0; i < 10; i++) {
+      $("#num" + i).addEventListener("click", (event) => {
+         manage_event("num", event.target.dataset.number);
+      });
+   }
+
+   // the dot for floats
+   $("#dot").addEventListener("click", (event) => {
+      manage_event("dot", event.target.dataset.symbol);
+   });
+
+   // specific event, because it also counts as a sign
+   $("#sub").addEventListener("click", (event) => {
+      manage_event("minus", event.target.dataset.symbol);
+   });
+
+   // other math operators that add only their symbols saved in the data attr
+   const other_operators = ["#add", "#mul", "#div"];
+   other_operators.forEach((op) => {
+      $(op).addEventListener("click", (event) => {
+         manage_event("operator", event.target.dataset.symbol);
+      });
+   });
+
+   // equal to only evaluate
+   $("#eq").addEventListener("click", (event) => {
+      manage_event("equal", "");
+   });
+
+   // all clear
+   $("#ac").addEventListener("click", (event) => {
+      manage_event("all_clear", "");
+   });
+}
+
+const calc_fsm = {
+   transitions: {
+      //#current state     new state      action to take
+      //------------------------------------------------
+      clear: {
+         minus: ["int_1", "add_first_char_to_display"],
+         num: ["int_1", "add_first_char_to_display"],
+      },
+      int_1: {
+         operator: ["operator", "add_to_display"],
+         minus: ["operator", "add_to_display"],
+         num: ["int_1", "add_to_display"],
+         dot: ["float_1", "add_to_display"],
+         all_clear: ["clear", "clear_all"],
+      },
+      float_1: {
+         operator: ["operator", "add_to_display"],
+         minus: ["operator", "add_to_display"],
+         num: ["float_1", "add_to_display"],
+         all_clear: ["clear", "clear_all"],
+      },
+      operator: {
+         minus: ["int_2", "add_to_display"],
+         num: ["int_2", "add_to_display"],
+         all_clear: ["clear", "clear_all"],
+      },
+      int_2: {
+         operator: ["operator", "calculate"],
+         equal: ["eval", "calculate"],
+         num: ["int_2", "add_to_display"],
+         dot: ["float_2", "add_to_display"],
+         all_clear: ["clear", "clear_all"],
+      },
+      float_2: {
+         operator: ["eval", "calculate"],
+         equal: ["eval", "calculate"],
+         num: ["float_2", "add_to_display"],
+         all_clear: ["clear", "clear_all"],
+      },
+      eval: {
+         operator: ["operator", "add_to_display"],
+         minus: ["operator", "add_to_display"],
+         all_clear: ["clear", "clear_all"],
+      },
+   },
+   state: "clear",
+   action: null,
+   default_action: "ignore",
+   accept_event(event) {
+      [this.state, this.action] = this.transitions[this.state][event] ||
+         this.transitions[this.state]["default"] || [this.state, this.default_action];
+      return this.action;
+   },
+};
+
+function manage_event(event, value) {
+   const action = calc_fsm.accept_event(event);
+   switch (action) {
+      case calc_fsm.default_action:
+         break;
+      case "add_first_char_to_display":
+         display.value = value;
+         break;
+      case "add_to_display":
+         display.value += value;
+         break;
+      case "calculate":
+         const [stat, result] = calculate(display.value);
+         display.value = result + value;
+         if (stat === "error") {
+            calc_fsm.state = "clear";
+         }
+         break;
+      case "clear_all":
+         display.value = "";
+         break;
+      default:
+         throw new Error(`No such case: "${action}"`);
+         break;
+   }
 }
 
 function calculate(string) {
@@ -29,17 +145,11 @@ function calculate(string) {
    return Number.isFinite(result) ? ok(result) : error("Result is not a number");
 }
 
-function is_valid_operation(string) {
-   const n_operands = 2;
-   const n_operators = 1;
-   const is_two_numbers = string.match(/\d*\.?\d+/g)?.length === n_operands;
-   const is_one_operator = string.match(/[-+*\/]/g)?.length === n_operators;
-   return is_two_numbers && is_one_operator;
-}
-
 function parse_math_operation(string) {
-   const numbers = string.match(/\d*\.?\d+/g).map(Number);
-   const operator = string.match(/[-+*\/]/)[0];
+   //first regex matches first number with sign and second matches second number with sign if there is an operator already
+   const numbers = string.match(/^-?\d*\.?\d+|(?<=[-+*/$])-?\d*\.?\d+/g).map(Number);
+   // takes the first operator that is preceded by a number
+   const operator = string.match(/(?<=\d)[-+*\/]/)[0];
    return [numbers, operator];
 }
 
@@ -66,3 +176,17 @@ function ok(result) {
 function error(reason) {
    return ["error", reason];
 }
+
+function add_to_display(value) {
+   add_to_output(value, display);
+}
+
+function add_to_output(value, node) {
+   node.value += value;
+}
+
+function do_nothing() {
+   //pass, to avoid calling error
+}
+
+bind_events();
