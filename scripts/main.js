b014c058d7657bd71fdbded4110e736c7aaf5fd9
BM <b.mungan@hotmail.de>
2022-06-18 23:45:52 +0200

    reduce global dependency

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,92 @@
 import { $ } from "./toolbox.mjs";
 /**
- * Bind the events to the calculator buttons and call the corresponding function
- * for the evaluation of the input value
+ * A finite state machine
+ * @typedef  {object}                                          FSM
+ * @property {{state_1:
+ *    {event_1: [next_state:string, action: string]}}}         transitions       The accepted events in a state, the next state and following action
+ * @property {string}                                          init_state        The initial / beginning state of the FSM
+ * @property {string}                                          state             The actual state of the FSM
+ * @property {string}                                          def_event         Default event in case other events aren't valid
+ * @property {string}                                          def_action        Defeault action in case there is no default event accepted
+ * @property {string[]}                                        tstates           Includes transition states where the created value is in a valid state
+ * @property {function(string):string}                         accept            Accepts an event, possibly changes its state and returns an action
+ * @property {function():undefined}                            reset             Sets the current state to the initial state
+ * @property {function():bool}                                 is_tstate         Checks if the current state is a transition state
  */
-function bind_events() {
+
+/**
+ * Named grouping (events) of characters
+ * @typedef {[{name: String, regex: Regex}]} char_events
+ */
+
+/**
+ * Bind the inputs and events with the functions
+ */
+function init_calculator() {
+   /**
+    * Define the characters that count as an event
+    * @type {char_events}
+    */
+   const char_events = [
+      { name: "num", regex: /\d/ },
+      { name: "del", regex: /d/ },
+      { name: "ac", regex: /a/ },
+      { name: "op", regex: /[+*/]/ },
+      { name: "minus", regex: /-/ },
+      { name: "dot", regex: /\./ },
+      { name: "eq", regex: /=/ },
+      { name: "base", regex: /e/ },
+   ];
+   /**
+    * An FSM for creating a number
+    * @type {FSM}
+    */
+   const num_fsm = {
+      transitions: {
+         init: {
+            minus: ["num_sign", "add_char"],
+            num: ["int", "add_char"],
+         },
+         num_sign: {
+            num: ["int", "add_char"],
+         },
+         int: {
+            num: ["int", "add_char"],
+            dot: ["float", "add_char"],
+            base: ["base", "add_char"],
+         },
+         float: {
+            num: ["float", "add_char"],
+         },
+         base: { minus: ["exp_sign", "add_char"], num: ["exp", "add_char"] },
+         exp_sign: {
+            num: ["exp", "add_char"],
+         },
+         exp: {
+            num: ["exp", "add_char"],
+         },
+      },
+      init_state: "init",
+      state: "init",
+      def_event: "def",
+      def_action: "ignore",
+      tstates: ["int", "float", "exp"],
+
+      accept(event) {
+         const [next_state, action] = this.transitions[this.state][event] ||
+            this.transitions[this.state][this.def_event] || [this.state, this.def_action];
+         this.state = next_state;
+         return action;
+      },
+
+      reset() {
+         this.state = this.init_state;
+      },
+
+      is_tstate() {
+         return this.tstates.includes(this.state);
+      },
+   };
    // textfield where the input and calculation is shown
    const display = $("#display");
    // accepted input buttons
@@ -30,7 +113,13 @@ function bind_events() {
    // bind calc function to the input buttons
    char_inputs.forEach((input) => {
       $(input.id).addEventListener("click", () => {
-         display.value = handle_input(input.value);
+         const curr_expr = display.value;
+         display.value = handle_input({
+            str: input.value,
+            expr: curr_expr,
+            char_events: char_events,
+            num_fsm: num_fsm,
+         });
       });
    });
 
@@ -45,37 +134,15 @@ function bind_events() {
    };
 }
 
-/**
- * Groups certain characters and gives them an event name
- * @typedef {[{name: String, regex: Regex}]} char_events
- */
-
-/**
- * Define the characters that count as an event
- * @type {char_events}
- */
-const defined_events = [
-   { name: "num", regex: /\d/ },
-   { name: "del", regex: /d/ },
-   { name: "ac", regex: /a/ },
-   { name: "op", regex: /[+*/]/ },
-   { name: "minus", regex: /-/ },
-   { name: "dot", regex: /\./ },
-   { name: "eq", regex: /=/ },
-   { name: "base", regex: /e/ },
-];
-
-// save the current expression
-let curr_expr = "";
-
 // evaluates the chars and current expression to create a math expression
 // and calculate it
-function handle_input(str) {
+function handle_input({ str, expr, char_events, num_fsm }) {
    const chars = str.split("");
    let event = "";
    let action = "";
+   let curr_expr = expr;
    chars.forEach((char) => {
-      event = get_char_event(char, defined_events);
+      event = get_char_event(char, char_events);
       action = num_fsm.accept(event);
       curr_expr = exe_action(action, char, curr_expr);
    });
@@ -100,77 +167,6 @@ function get_char_event(char, events) {
    return char_event;
 }
 
-/**
- * A finite state machine
- * @typedef  {object}                                          FSM
- * @property {{state_1:
- *    {event_1: [next_state:string, action: string]}}}         transitions       The accepted events in a state, the next state and following action
- * @property {string}                                          init_state        The initial / beginning state of the FSM
- * @property {string}                                          state             The actual state of the FSM
- * @property {string}                                          def_event         Default event in case other events aren't valid
- * @property {string}                                          def_action        Defeault action in case there is no default event accepted
- * @property {string[]}                                        tstates           Includes transition states where the created value is in a valid state
- * @property {function(string):string}                         accept            Accepts an event, possibly changes its state and returns an action
- * @property {function():undefined}                            reset             Sets the current state to the initial state
- * @property {function():bool}                                 is_tstate         Checks if the current state is a transition state
- */
-
-/**
- * An FSM for creating a number
- * @type {FSM}
- */
-
-const num_fsm = {
-   transitions: {
-      init: {
-         minus: ["num_sign", "add_char"],
-         num: ["int", "add_char"],
-      },
-      num_sign: {
-         num: ["int", "add_char"],
-      },
-      int: {
-         num: ["int", "add_char"],
-         minus: ["init", "as_op"],
-         dot: ["float", "add_char"],
-         base: ["base", "add_char"],
-      },
-      float: {
-         num: ["float", "add_char"],
-         minus: ["init", "as_op"],
-      },
-      base: { minus: ["exp_sign", "add_char"], num: ["exp", "add_char"] },
-      exp_sign: {
-         num: ["exp", "add_char"],
-      },
-      exp: {
-         num: ["exp", "add_char"],
-         minus: ["init", "as_op"],
-      },
-   },
-   init_state: "init",
-   state: "init",
-   def_event: "def",
-   def_action: "ignore",
-   tstates: ["int", "float", "exp"],
-
-   accept(event) {
-      console.log(event, this.state);
-      const [next_state, action] = this.transitions[this.state][event] ||
-         this.transitions[this.state][this.def_event] || [this.state, this.def_action];
-      this.state = next_state;
-      return action;
-   },
-
-   reset() {
-      this.state = this.init_state;
-   },
-
-   is_tstate() {
-      return this.tstates.includes(this.state);
-   },
-};
-
 // execute the given action
 function exe_action(action, char, expression) {
    let expr = expression;
@@ -187,4 +183,4 @@ function exe_action(action, char, expression) {
 }
 
 //main
-bind_events();
+init_calculator();
