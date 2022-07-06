064b9fa41cd497dd7d4ef1c586e158e2caf380d0
LittleArmstrong <59065008+LittleArmstrong@users.noreply.github.com>
2022-07-06 17:32:46 +0200

    add some tests

--- test/test.js
+++ test/test.js
@@ -1,14 +1,21 @@
 import { calculate } from "../scripts/calculator.mjs";
 
-// the tests for the calculate function
+// some tests for the calculate function
 const tests = [
-   test("no multiple signs or dots", () => calculate("-.-12..3.4.")[0], "-12.34"),
-   test("addition", () => calculate("--12..3.4+-15..4.3=")[0], "-27.77"),
+   test("valid number", () => calculate("e-.-12..3e-e.4.e")[0], "-12.3e-4"), //check num fsm
+   test("addition", () => calculate("-12.34+-15.43=")[0], "-27.77"),
    test("subtraction", () => calculate("11-10=")[0], "1"),
    test("multiplication", () => calculate("-10*-10=")[0], "100"),
    test("division", () => calculate("99/33=")[0], "3"),
    test("calculate multiple times", () => calculate("-1+2*3/3=")[0], "1"),
    test("calculation with next op", () => calculate("-1+2-")[0], "1-"),
+   test("num input on calculated num", () => calculate("-1+2=5")[0], "5"),
+   test("divide by zero error", () => calculate("1/0=")[0], "ERR"),
+   test("num input on error", () => calculate("1/0=3")[0], "3"),
+   test("reset num fsm for second num", () => calculate("1/")[2], "init"),
+   test("reset num fsm for second num afte calc", () => calculate("1/2=+")[2], "init"),
+   test("clear display", () => calculate("1/2=+a")[0], ""),
+   test("delete last char", () => calculate("1/2d")[0], "1/"),
 ];
 console.table(tests);
 
